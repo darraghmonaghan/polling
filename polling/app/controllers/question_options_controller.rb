43682dd9282d70ca61e48be5da94004104f16e27
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-10-23 12:38:54 -0700

    question answer options being uploaded, not linked to a question yet though

--- polling/app/controllers/question_options_controller.rb
+++ polling/app/controllers/question_options_controller.rb
@@ -4,6 +4,18 @@ class QuestionOptionsController < ApplicationController
 		@options = QuestionOption.new
 	end
 
+	def create
+		@option = QuestionOption.create(option_params)
+		redirect_to root_path
+	end
+
+
+
+  private
+
+  def option_params
+    params.require(:question_option).permit(:option)  
+  end
 
 
 end
