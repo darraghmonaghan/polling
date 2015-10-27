d710bdd823ed6d317189fd6b4d85166de076a266
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-10-26 18:14:41 -0700

    updated question index page

--- polling/app/models/question.rb
+++ polling/app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ActiveRecord::Base
 
 	belongs_to :user
-	has_many :user_inputs # plural!!!
-	has_many :question_options, dependent: :destroy #plural!!!!
+	has_many :user_inputs
+	has_many :question_options, dependent: :destroy
 
 	accepts_nested_attributes_for :question_options, :reject_if => lambda { |a| a[:option].blank? }, :allow_destroy => true
 
