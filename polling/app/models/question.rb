33edd2ecb0d9d2d9b5e8a171cce2279da825751f
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-10-28 19:07:33 -0700

    homepage rendering properly

--- polling/app/models/question.rb
+++ polling/app/models/question.rb
@@ -6,8 +6,8 @@ class Question < ActiveRecord::Base
 
 	accepts_nested_attributes_for :question_options, :reject_if => lambda { |a| a[:option].blank? }, :allow_destroy => true
 
-	def options
-		question_options
-	end
+	# def options
+	# 	question_options
+	# end
 
 end
